fbdef14914ae62a6b6b218cc7d5f7b481013ecc8
ClavdiaJFA <claudiajfloresaguilera@gmail.com>
2023-01-30 22:25:17 -0600

    1st-view

---
+++ main.js
@@ -0,0 +1,13 @@
+$("path, circle").hover(function(e) {
+    $('#info-box').css('display','block');
+    $('#info-box').html($(this).data('info'));
+  });
+  
+  $("path, circle").mouseleave(function(e) {
+    $('#info-box').css('display','none');
+  });
+  
+  $(document).mousemove(function(e) {
+    $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+    $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+  }).mouseover();
